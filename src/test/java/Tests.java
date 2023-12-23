0151d289fc4b2fd9a308967e44af63d2e82ce0c9
Kokecena <jovanni_n@live.com.mx>
2023-12-23 06:27:54 -0600

    Corregido el caso en que no se encuentre ninguna cancion.

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,7 +1,10 @@
 import com.github.kokecena.skymus.service.SkymusServiceImplements;
+import reactor.core.publisher.Mono;
 
 public class Tests {
     public static void main(String[] args) {
-        new SkymusServiceImplements().getTracks("eurobeat").subscribe(System.out::println);
+        new SkymusServiceImplements().getTracks("eurobeatasdasdasasdasdasda")
+                .switchIfEmpty(Mono.fromRunnable(() -> System.out.println("Vacio")))
+                .subscribe(System.out::println);
     }
 }
